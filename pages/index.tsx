96013ee975f962bc216f7fd90057f811c6e1b697
stephenm7777 <stephenmartinez7000@gmail.com>
2023-06-21 20:35:27 -0700

    updated files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
+import React, {useState} from 'react'; 
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+// Component
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GenerateQuoteButton, GenerateQuoteButtonText, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorSubTitle, QuoteGeneratorTitle } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 
+// Assets 
+import Cloud1 from '@/assets/cloudy-weather.png'
+import Cloud2 from '@/assets/cloud-and-thunder.png'
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -14,8 +23,49 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/*background*/}
+      {/* Quote Generator Modal pop uo*/}
+      { /* <QuoteGeneratorModal /> */}
+
+      <QuoteGeneratorCon>
+        <QuoteGeneratorInnerCon>
+          <QuoteGeneratorTitle>
+            Daily Inspiration Generator
+          </QuoteGeneratorTitle>
+
+          <QuoteGeneratorSubTitle>
+            Looking for a splash of inspiration? Generate a quote card with a 
+            random inspirational quote provided by <FooterLink href="https://zenquotes.com.io/"
+            target="_blank" rel ="noopener noreferrer">ZenQuotesAPI</FooterLink>
+          </QuoteGeneratorSubTitle>
+          <GenerateQuoteButton>
+            <GenerateQuoteButtonText onClick={null}>
+              Make a Quote
+            </GenerateQuoteButtonText>
+          </GenerateQuoteButton>
+        </QuoteGeneratorInnerCon>
+      </QuoteGeneratorCon>
+
+
+
       <GradientBackgroundCon>
-        
+        <BackgroundImage1
+          src = {Cloud1}
+          height = "300"
+          alt = "cloudybackground1"
+          />
+        <BackgroundImage2
+          src = {Cloud2}
+          height= "300"
+          alt = "cloudybackground2"
+        />
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed by <FooterLink href="https://www.linkedin.com/in/stephen-martinez-043553257/"
+            target = "_blank" rel="noopener noreferrer"> @StephenMartinez </FooterLink>
+          </>
+        </FooterCon>
       </GradientBackgroundCon>
     </>
   )
