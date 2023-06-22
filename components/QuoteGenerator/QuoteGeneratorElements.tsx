257e884757d83ff17c9d73ead941c9cfc1e7e24f
stephenm7777 <stephenmartinez7000@gmail.com>
2023-06-21 20:52:41 -0700

    final upload

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -1,58 +1,59 @@
 import styled from "styled-components";
-import Link from "next/link"; 
+import Link from "next/link";
 import Image from "next/image";
-import { execSync } from "child_process";
-
+import { Box, CircularProgress } from "@mui/material";
 
 export const GradientBackgroundCon = styled.div`
-    background: linear-gradient(to right, #000046, #1CB5E0); // switching between these two colors 
-    background-size: 400% 400%;
-    animation: gradient 6s ease infinite; 
-    height: 100vh; // fit to screen
-    width: 100vw;  // fit to screen
-    @keyframes gradient {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
+  background: linear-gradient(to right, #000046, #1cb5e0);
+  background-size: 400% 400%;
+  animation: gradient 6s ease infinite;
+  height: 100vh;
+  width: 100vw;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
     }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
 `;
 
-export const BackgroundImage1  = styled(Image)`
-    position: relative;
-    z-index: 1;
-    margin-left: -125px; 
-    margin-top: -10px;
+export const BackgroundImage1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  margin-left: -10px;
+  margin-top: -10px;
 `;
 
 export const BackgroundImage2 = styled(Image)`
-    position: fixed;
-    z-index: 1;
-    right: 18px; 
-    bottom: -10px;
+  position: fixed;
+  z-index: 1;
+  right: -120px;
+  bottom: -10px;
 `;
 
 export const FooterCon = styled.div`
-    width: 100vw; 
-    height: 50px; 
-    text-align: center;
-    font-family: "Source Code Pro", monospace; 
-    font-size: 15px; 
-    position: absolute;
-    bottom: 0; 
-    color: white; 
-    z-index: 999999; 
-
+  width: 100vw;
+  height: 50px;
+  text-align: center;
+  font-family: 'Source Code Pro', monospace;
+  font-size: 15px;
+  position: absolute;
+  bottom: 0;
+  color: white;
+  z-index: 999999;
 `;
 
+export const RedSpan = styled.span`
+    color: red;
+`;
 
 export const FooterLink = styled(Link)`
-    color: white; 
+    color: white;
 `;
 
 export const QuoteGeneratorCon = styled.div`
@@ -83,6 +84,7 @@ export const QuoteGeneratorInnerCon = styled.div`
     position: absolute;
     width: 100%;
 `;
+
 export const QuoteGeneratorTitle = styled.div`
     font-family: 'Permanent Marker', cursive;
     font-size: 50px;
@@ -137,15 +139,55 @@ export const GenerateQuoteButton = styled.div`
         transform-origin: center;
     }
 `;
+
 export const GenerateQuoteButtonText = styled.div`
-    color: white; 
-    font-family: 'Caveat', cursive; 
-    font-size: 35px; 
-    left: 50%; 
-    top: 50%; 
-    transform: translate(-50%, -50%); 
+    color: white;
+    font-family: 'Caveat', cursive;
+    font-size: 35px;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
     position: absolute;
     width: 100%;
     text-align: center;
+`;
 
+export const QuoteGeneratorModalCon = styled(Box)`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 70vw;
+    height: 70vh;
+    box-shadow: 24;
+    /* transition: 0.2s all ease-in-out; */
+
+    background: rgb(193 193 255 / 19%);
+    box-shadow: 0 8px 32px 0 rgb(31 38 135 / 37%);
+    -webkit-backdrop-filter: blur( 20px );
+    backdrop-filter: blur( 20px );
+    -webkit-backdrop-filter: blur( 20px );
+    border-radius: 10px;
+    border: 1px solid rgba( 255,255,255,0.18 );
+
+    &:focus {
+      outline: none !important;
+    }
+`;
+
+export const QuoteGeneratorModalInnerCon = styled.div`
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    position: relative;
+`;
+
+export const ModalCircularProgress = styled(CircularProgress)`
+    color: white !important;
+    stroke-linecap: round;
+    position: relative;
+    /* to account for the larger element pushing left */
+    margin-left: -55px;
+    left: 50%;
+    transform: translateX(-50%);
 `;
